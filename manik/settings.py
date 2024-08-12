47f52d7ae907e6258d283238a78e202f31a4f303
Nikhil-Yadav-73 <nikhilyadavwork73@gmail.com>
2024-08-12 11:21:59 +0530

    -- heroku

--- manik/settings.py
+++ manik/settings.py
@@ -1,4 +1,3 @@
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,5 +124,3 @@ STATIC_ROOT = BASE_DIR / 'website/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
