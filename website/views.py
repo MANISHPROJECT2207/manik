dc5f7ca3a4f11db068c11477cc99442cd0349c3d
Nikhil-Yadav-73 <nikhilyadavwork73@gmail.com>
2024-08-21 20:45:09 +0530

    scroller index

--- website/views.py
+++ website/views.py
@@ -31,10 +31,50 @@ def about(request):
 def test(request):
     return render(request, 'test.html')
 
+# branch_dict = {}
+#     for branch_code, branch_name in Subject._meta.get_field('branch').choices:
+#         subjects = Subject.objects.filter(branch=branch_code, year=year)
+#         subject_list = []
+#         for subject in subjects:
+#             total_units = subject.units.count()
+#             completed_units = subject.units.filter(completed_by=request.user).count()
+        
+#             if total_units > 0: progress = (completed_units / total_units) * 100
+#             else: progress = 0
+            
+#             subject_data = {
+#                 'subject': subject,
+#                 'progress': progress
+#             }
+#             subject_list.append(subject_data)
+        
+#         branch_dict[branch_name] = subject_list
+
 def home(request):
     top_subjects = Subject.objects.order_by('-views')[:3]
     total_users = User.objects.all().count()
-    return render(request, 'index.html', {'top_subjects':top_subjects, 'total_users':total_users})
+    popular = Subject.objects.all().order_by('views')[:8]
+    subject_list = []
+    for subject in popular:
+            total_units = subject.units.count()
+            completed_units = subject.units.filter(completed_by=request.user).count()
+        
+            if total_units > 0: progress = int((completed_units / total_units) * 100)
+            else: progress = 0
+            
+            subject_data = {
+                'subject': subject,
+                'progress': progress
+            }
+            subject_list.append(subject_data)
+    print(subject_list)
+    
+    return render(request, 'index.html', {
+        'top_subjects':top_subjects,
+        'total_users':total_users,
+        'popular':popular,
+        'subject_list':subject_list,
+        })
 
 def testimonial(request):
     return render(request, 'testimonial.html')
@@ -54,7 +94,7 @@ def profile(request):
             completed_units = subject.units.filter( completed_by=request.user).count()
          
             if total_units > 0:
-                progress = (completed_units / total_units) * 100
+                progress = int((completed_units / total_units) * 100)
             else:
                 progress = 0
             
@@ -321,10 +361,8 @@ def year(request, year):
             total_units = subject.units.count()
             completed_units = subject.units.filter(completed_by=request.user).count()
         
-            if total_units > 0:
-                progress = (completed_units / total_units) * 100
-            else:
-                progress = 0
+            if total_units > 0: progress = int((completed_units / total_units) * 100)
+            else: progress = 0
             
             subject_data = {
                 'subject': subject,
@@ -333,7 +371,6 @@ def year(request, year):
             subject_list.append(subject_data)
         
         branch_dict[branch_name] = subject_list
-    
     a = Subject.objects.all().filter(year = 1)
     b = Subject.objects.all().filter(year = 2)
     c = Subject.objects.all().filter(year = 3)
