3ed68e405ebfac816d597606428697ce78f2d387
Nikhil-Yadav-73 <nikhilyadavwork73@gmail.com>
2024-08-22 17:27:32 +0530

    api app --

--- manik/settings.py
+++ manik/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
-    'api',
 ]
 
 MIDDLEWARE = [
--- manik/urls.py
+++ manik/urls.py
@@ -6,5 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("website.urls")),
-    path('buy-me-a-coffee/', include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
