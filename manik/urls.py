0646619500f7f05a55a84ce9d0d52fce954d09b3
Nikhil-Yadav-73 <nikhilyadavwork73@gmail.com>
2024-08-22 12:13:54 +0530

    new app razorpay

--- manik/urls.py
+++ manik/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("website.urls")),
+    path('buy-me-a-coffee/', include("razorpay.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
